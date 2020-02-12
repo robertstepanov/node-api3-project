a1dffd4bd365a32651295c95f0d44c0e7b0a63ba
Robert Stepanov <robertdstepanov@gmail.com>
2020-02-12 17:42:54 -0500

    Just middleware left

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code away!
+const express = require('express');
+
+const server = require("./server");
+
+// const userRouter = require('./users/userRouter');
+
+// const server = express();
+
+// server.use(express.json());
+
+// server.use('/api/users', userRouter)
+
+server.listen(4000, () => {
+  console.log("\n*** Server is running on localhost:4000 **\n");
+});
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,33 +1,96 @@
-const express = require('express');
+const express = require("express");
+
+const Users = require("./userDb");
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // do your magic!
+  Users.insert(req.body)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: "Error adding user" });
+    });
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post("/:id/posts", (req, res) => {
   // do your magic!
+  const { id } = req.params;
+  const post = { ...req.body, post_id: id };
+
+  Users.insert(post)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: "Error adding user" });
+    });
 });
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // do your magic!
+  Users.get(req.query)
+    .then(post => {
+      res.status(200).json(post);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: "Error retrieving users" });
+    });
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // do your magic!
+  Users.getById(req.params.id)
+    .then(post => {
+      res.status(200).json(post);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: "No user with that ID" });
+    });
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get("/:id/posts", (req, res) => {
   // do your magic!
+  Users.getUserPosts(req.params.id)
+    .then(post => {
+      res.status(200).json(post);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: "No user with that ID" });
+    });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // do your magic!
+  Users.remove(req.params.id)
+    .then(removed => {
+      res.status(200).json(removed);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: "Error removing user" });
+    });
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // do your magic!
+  const { id } = req.params;
+  const changes = req.body;
+  Users.update(id, changes)
+    .then(post => {
+      res.status(200).json(post);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: "Error updating the user" });
+    });
 });
 
 //custom middleware
